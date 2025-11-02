47d513f87090b50ca726fd6430e28c6b2250f50b
Bennett Petzold <dansecob.dev@gmail.com>
2025-11-02 16:33:21 -0500

    Change from notify to simple

---
+++ yocto/scripts/flash_image.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -xeo pipefail
+
+echo 'This assumes /dev/mmcblk0 is the target media!'
+echo "It makes a lot of assumptions based on the author's machine."
+
+# https://stackoverflow.com/a/34642589
+if (return 2>/dev/null); then
+	echo 'This script should not be sourced!'
+	exit 1
+fi
+
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+cd "$SCRIPT_DIR" || return
+./make_image.bash "$@"
+
+pv -o /dev/mmcblk0 "$(find ../build/wic-builds/ | grep -m 1 -v '\.p.*$')"
