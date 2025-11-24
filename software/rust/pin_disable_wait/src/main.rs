2d010d845a7bc54a328c31ea12ddfdd23afec88e
Bennett Petzold <dansecob.dev@gmail.com>
2025-11-24 13:55:52 -0500

    Add pin event time checks

--- software/rust/pin_disable_wait/src/main.rs
+++ software/rust/pin_disable_wait/src/main.rs
@@ -1,9 +1,17 @@
 use std::env::args;
 
-use gpio_cdev::{Chip, EventRequestFlags, LineRequestFlags};
+use gpio_cdev::{Chip, EventRequestFlags, EventType, LineRequestFlags};
+use nix::{
+    sys::time::TimeValLike,
+    time::{ClockId, clock_gettime},
+};
 
 /// Waits to exit until the GPIO line given as the only argument to is disabled.
 fn main() {
+    let start_time = clock_gettime(ClockId::CLOCK_MONOTONIC)
+        .expect("Well defined Linux call")
+        .num_milliseconds() as u64;
+
     let mut chip = Chip::new(option_env!("GPIO_CHIP").unwrap_or("/dev/null")).unwrap();
 
     let line = chip
@@ -20,6 +28,11 @@ fn main() {
 
     // Exit on either false or first falling edge.
     if events.get_value().unwrap() == 0 {
-        let _ = events.next();
+        loop {
+            let event = events.next().expect("GPIO events are infinite").unwrap();
+            if event.event_type() == EventType::FallingEdge && event.timestamp() >= start_time {
+                break;
+            }
+        }
     }
 }
